20a334f85dec9cf4a00a29c27550b9fc32a649fa
OrichOrech <touchthiscant90@gmail.com>
2026-06-07 15:12:36 +0300

    Modify NuhaLogo component className and style
    Updated default className for NuhaLogo component and added objectFit style.

--- src/components/NuhaLogo.tsx
+++ src/components/NuhaLogo.tsx
@@ -1,11 +1,12 @@
 import logo from '../../assets/nuha-logo.png';
 
-export default function NuhaLogo({ className = "w-10 h-10", iconOnly = false }: NuhaLogoProps) {
+export default function NuhaLogo({ className = "h-12 w-auto" }: NuhaLogoProps) {
   return (
     <img
       src={logo}
       alt="Northern Uganda Hoteliers Association"
       className={className}
+      style={{ objectFit: 'contain' }}
     />
   );
 }
